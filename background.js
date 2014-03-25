ab2f97c7e738a0dfe13c7cd609312cd7c68b485f
Christian Calloway <ccalloway@cancapital.com>
2014-03-24 22:45:37 -0400

    interval on reload working

--- background.js
+++ background.js
@@ -3,13 +3,31 @@ chrome.windows.onCreated.addListener(function(w) {
   //chrome.tabs.query({'active': true}, function(tabs) {
     //setTimeout(function() { 
     chrome.windows.get(w.id, { "populate": true }, function(w) {
-      //chrome.tabs.update(w.tabs[0].id, { "url": "http://www.elmundodigital.net"})
+      chrome.tabs.update(w.tabs[0].id, { "url": "http://www.elmundodigital.net"})
+
+      f = function(tab_id) {
+        var counter = 0
+
+        setInterval(function() {
+
+          
+          chrome.tabs.reload(tab_id, { }, function() { })
+
+        }, 10000)
+      }
+      f(w.tabs[0].id)
+      return 
 
       chrome.tabs.onUpdated.addListener(function(id, changed, tab) {
-        chrome.cookies.getAll({domain: "whatarecookies.com"}, function(cookies) {
+
+        // remove all existing cookies
+        // TODO: we will need to target only flash related cookies - 
+        // maybe figure out a way of determining which cookies have
+        // since been added
+        chrome.cookies.getAll({}, function(cookies) {
           for(var i = 0; i < cookies.length; i++) {
             chrome.cookies.remove({
-              url: "http://www.whatarecookies.com" + cookies[i].path, 
+              url: "http://" + cookies[i].domain + cookies[i].path, 
               name: cookies[i].name
             
             }, function(details) {
